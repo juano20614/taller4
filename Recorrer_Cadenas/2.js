2bb23e6d551be6ffb586660e04e83f773f1e20d7
juano20614 <134510225+juano20614@users.noreply.github.com>
2023-06-28 17:41:44 -0500

    Add files via upload

---
+++ Recorrer_Cadenas/2.js
@@ -0,0 +1,29 @@
+// 2.Dada la cadena “Somos SENA”:
+// c) Recorra la cadena usando ciclo for y muestre si la cadena contiene la letra “E”
+// d) Recorra la cadena usando ciclo for y muestre cuántas veces está la letra “o” . Como sugerencia use una
+// variable contadora para contar el número de veces que se encuentra la letra “o”.
+
+let cade="Somos SENA";
+
+for (let i = 0; i < cade.length; i++) {
+
+   if (cade[i].includes("E")) {
+      console.log("si"); 
+   }
+}
+
+
+let c = 0;
+let cadem = "Somos SENA";
+
+for (let i = 0; i < cadem.length; i++) {
+    if (cadem[i]=="o") {
+        c++
+    }}
+console.log(c);
+
+
+
+
+
+
---
+++ Recorrer_Cadenas/3.js
@@ -0,0 +1,8 @@
+// 3.Dada la cadena “Javascript es genial” recorra la cadena usando ciclo for y muestre por cuantas palabras está
+// compuesta la cadena. Como sugerencia se puede basar en el modelo del ejercicio 2 literal d.
+
+let cade="Javascript es genial";
+
+for (let i = 0; i < cade.length; i++) {
+
+}console.log(cade.length);
